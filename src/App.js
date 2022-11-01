5fc0cbceabd1e56a0404fe54a6e3dd85e545f11d
matte-134 <matthew.eland@nandos.co.uk>
2022-11-01 11:45:51 +0000

    fix bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TypesExample from './components/SearchBar';
+import "bootstrap/dist/css/bootstrap.css"
 
 function App() {
   return (
     <div>
-      Hello
+      
     </div>
   );
 }
