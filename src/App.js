11a942987cabd84c3212e333c5c96015a0672faa
Abdalla Yusuf <me@a135y.com>
2022-11-14 13:38:55 +0000

    Navbar started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { GameDetails } from './routes/gameDetails';
-import TypesExample from './components/SearchBar';
 import "bootstrap/dist/css/bootstrap.css"
-
-
 import React, { useEffect, useState } from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import { Card } from "react-bootstrap";
+import { TheNavbar } from './components/NavBar';
+
 function App() {
 
   return (
     <div>
-      <h1>Game Details</h1>
+      <TheNavbar />
       <GameDetails />
     </div>
   )
