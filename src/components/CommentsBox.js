369eafc73db7409d203a3749a1874b7798c5c736
xryno <ryan.curry@outlook.com>
2022-11-18 21:18:13 +0000

    more styling

--- src/components/CommentsBox.js
+++ src/components/CommentsBox.js
@@ -8,9 +8,10 @@ export default function CommentsBox() { //pass gameId here as props
     { id: 2, rating: 4, userName: "OrangeTuna", comments: "lots of fun", date: "07/05/2022" },
     { id: 3, rating: 3, userName: "DogBlue", comments: "i didnt enjoy it", date: "30/10/2022" },
   ]);
-  const [rating, setRating] = useState("Select Rating");
+  const [rating, setRating] = useState("0");
   const [comments, setComments] = useState("");
-  let [averageRating, setAverageRating] = useState(0)
+  const [isChecked, setIsChecked] = useState(true)
+  const [averageRating, setAverageRating] = useState(0)
 
   useEffect(() => {
     const getRating = () => {
@@ -22,7 +23,6 @@ export default function CommentsBox() { //pass gameId here as props
           total += parseInt(each.rating)
           count ++
         }
-        
       })
         setAverageRating ((total/count).toFixed(2))
       return averageRating
@@ -31,19 +31,19 @@ export default function CommentsBox() { //pass gameId here as props
   }, [commentData])
   
   const userNameMaker = () => {
-    let a = ["Small", "Blue", "Black", "Jimbo", "Rocket", "Car", "Moon", "Cup", "Rose"];
-    let b = ["Bear", "Dog", "Banana", "Lion", "Sauce", "Pink", "Guitar", "Man", "Spider"];
-    let c = ["Christmas", "Berry", "99", "22", "111", "Mountain", "Lake", "Bath"];
+    const a = ["Small", "Blue", "Black", "Fast", "Rocket", "Car", "Moon", "Cup", "Rose", "Orange", "Custard"];
+    const b = ["Bear", "Dog", "Banana", "Cucumber", "Lion", "Sauce", "Pink", "Guitar", "Man", "Spider", "Cream"];
+    const c = ["Christmas", "Berry", "99", "22", "111", "Mountain", "Lake", "Bath", "Tuna"];
 
-    let rA = Math.floor(Math.random() * a.length);
-    let rB = Math.floor(Math.random() * b.length);
-    let rC = Math.floor(Math.random() * c.length);
-    let name = a[rA] + b[rB] + c[rC];
-    return name;
+    const rA = Math.floor(Math.random() * a.length);
+    const rB = Math.floor(Math.random() * b.length);
+    const rC = Math.floor(Math.random() * c.length);
+    const userName = a[rA] + b[rB] + c[rC];
+    return userName;
   };
 
   const handleClick = () => {
-    if (rating === "Select Rating") {
+    if (rating === "0") {
       alert("Please select a rating");
     } else {
       const now = Date.now();
@@ -60,61 +60,67 @@ export default function CommentsBox() { //pass gameId here as props
 
       setCommentData([...commentData, newComment]);
       setComments("");
-      setRating("Select Rating");
+      setRating("0");
     }
+    setIsChecked(true)
   };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value)
+    setIsChecked(false)
+  }
   
         
   return (
     <>
       <div className="d-flex flex-column w-75 p-1">
+        <h5 style={{ color: "white" }}>Overall rating: {averageRating}</h5>
         <div>
           <textarea
-            className="form-control"
+            className="comments"
             placeholder="Leave a review..."
             style={{ backgroundColor: "#6e9095", height: "6em", marginBottom: "1em" }}
             onChange={(e) => setComments(e.target.value)}
             value={comments}
           ></textarea>
+
           <div className="d-flex justify-content-end pb-3 pt-2">
-            <select
-              className="form-control"
-              style={{ backgroundColor: "#6e9095", marginRight: "1em", width: "8em" }}
-              onChange={(e) => setRating(e.target.value)}
-              value={rating}
-            >
-              <option disabled value="Select Rating">
-                Select Rating:
-              </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+            <div className="rating">
+              <input type="radio" id="star5" name="rating" value="5" onClick={(e) => handleRatingChange(e)} />
+              <label for="star5"></label>
+              <input type="radio" id="star4" name="rating" value="4" onClick={(e) => handleRatingChange(e)} />
+              <label for="star4"></label>
+              <input type="radio" id="star3" name="rating" value="3" onClick={(e) => handleRatingChange(e)} />
+              <label for="star3"></label>
+              <input type="radio" id="star2" name="rating" value="2" onClick={(e) => handleRatingChange(e)} />
+              <label for="star2"></label>
+              <input type="radio" id="star1" name="rating" value="1" onClick={(e) => handleRatingChange(e)} />
+              <label for="star1"></label>
+              <input type="radio" id="star0" name="rating" value="0" checked={isChecked} />
+              <label for="star0" style={{ display: "none" }}></label>
+              <h6 style={{ color: "white", paddingTop: "0.5em"}}>Your rating:</h6>
+            </div>
             <button className="btn btn-dark w-25" style={{ maxWidth: "8em" }} onClick={handleClick}>
               Submit
             </button>
           </div>
         </div>
-        
-        <h5 style={{color: 'white'}}>Overall rating: {averageRating}</h5>
+
         {commentData.map((each) => {
-          
           if (each.id === gameId) {
             return (
               <>
-               
-                <div className="p-2 mb-2 mt-2" style={{ backgroundColor: "#6e9095", borderRadius: "5px" }}>
+                <div className="comments" style={{  background: "linear-gradient(135deg, rgba(2,0,36,1) 0%, rgba(57,131,135,1) 47%, rgba(32,108,124,1) 100%);borderRadius: '5px'" }}>
                   <h6>{each.userName}</h6>
                   {each.comments}
                   <h6>
                     <div className="d-flex justify-content-between pt-3">
-                      <div className>Rating: {each.rating}</div>
+                      <div>Rating: {each.rating} out of 5</div>
                       {each.date}
                     </div>
                   </h6>
                 </div>
+                
               </>
             );
           }
