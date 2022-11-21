fceff02efe11a8303a979a14e831ca6a7d8b5c45
xryno <ryan.curry@outlook.com>
2022-11-21 21:39:33 +0000

    css changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import { GameDetails } from './routes/gameDetails';
 import "bootstrap/dist/css/bootstrap.css"
 import React, { useEffect, useState } from "react"
 import "bootstrap/dist/css/bootstrap.css"
-import CommentsBox from './components/CommentsBox'
 
 function App() {
 
   return (
     <>
-      {/* <GameDetails /> */}
-        <CommentsBox />
-        
+      <GameDetails />
     </>
   )
 }
