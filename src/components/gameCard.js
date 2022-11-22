e74624edd2a67945d74b7a8c4601d95b6953c8df
Abdalla Yusuf <me@a135y.com>
2022-11-22 09:50:15 +0000

    NavBar Update

--- src/components/gameCard.js
+++ src/components/gameCard.js
@@ -9,20 +9,20 @@ import { Card } from "react-bootstrap";
 
 function GameCard({ game }) {
 
-    return (
-        <div>
-            {game.map(game =>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={game.thumbnail} />
-                    <Card.Body>
-                        <Card.Title>{game.title}</Card.Title>
-                        <Card.Text>
-                            {game.short_description}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>)}
-        </div>
-    )
+  return (
+    <div>
+      {game.map(game =>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={game.thumbnail} />
+          <Card.Body>
+            <Card.Title>{game.title}</Card.Title>
+            <Card.Text>
+              {game.short_description}
+            </Card.Text>
+          </Card.Body>
+        </Card>)}
+    </div>
+  )
 }
 
 export { GameCard }
